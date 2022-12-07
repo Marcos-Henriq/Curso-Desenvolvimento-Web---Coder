2225f9cbd41061b83d2569fbfcc2ef26c315a93e
Marcos-Henriq <mh7505570@gmail.com>
2022-12-07 15:41:34 -0300

    adiciona alguns arquivos do capitulo de funções

---
+++ exercicio-js/funcao/factory1.js
@@ -0,0 +1,21 @@
+const prod1 = {
+    nome: '...',
+    preco: 45
+}
+const prod2 = {
+    nome: '...',
+    preco: 45
+}
+const prod3 = {
+    nome: '...',
+    preco: 45
+}
+
+// Factory simples
+
+function criarPessoa(){
+    return{
+        nome: 'Ana',
+        sobrenome:'Silva'
+    }
+}
---
+++ exercicio-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,15 @@
+// function declaration
+// pode ser executada antes memso de ser criada
+function soma(x,y){
+    return x + y;
+}
+
+//só pode ser executada após sua criação
+const soma = function(x,y){
+    return x - y;
+}
+
+//só pode ser executada após sua criação
+const mult = function mult(x,y){
+    return x+y;
+}
